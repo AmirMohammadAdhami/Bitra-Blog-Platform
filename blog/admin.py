479f91548095b83f91e46566481dabcf1ffef527
Amir Mohammad Adhami <amiradhamir@gmail.com>
2026-07-12 19:50:44 +0330

    feat : complete admin.py in accounts and blog app fix : add created_at and updated_at in article model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,120 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Category,
+    Tag,
+    Article,
+    ArticleView,
+    Comment,
+    CommentLike,
+)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+
+    search_fields = (
+        'name',
+    )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+
+    search_fields = (
+        'name',
+    )
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'title',
+        'author',
+        'category',
+        'status',
+        'created_at',
+    )
+
+    list_filter = (
+        'status',
+        'category',
+    )
+
+    search_fields = (
+        'title',
+        'author__username',
+        'content',
+    )
+
+    filter_horizontal = (
+        'tags',
+    )
+
+    readonly_fields = (
+        'likes',
+        'views',
+    )
+
+
+@admin.register(ArticleView)
+class ArticleViewAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'article',
+        'ip_address',
+        'created_at',
+    )
+
+    search_fields = (
+        'article__title',
+        'ip_address',
+    )
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'article',
+        'author',
+        'status',
+        'parent',
+        'created_at',
+    )
+
+    list_filter = (
+        'status',
+    )
+
+    list_editable = ['status']
+
+    search_fields = (
+        'content',
+        'author__username',
+        'article__title',
+    )
+
+
+@admin.register(CommentLike)
+class CommentLikeAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'user',
+        'comment',
+        'created_at',
+    )
+
+    search_fields = (
+        'user__username',
+        'comment__content',
+    )
--- blog/models.py
+++ blog/models.py
@@ -55,6 +55,10 @@ class Article(models.Model):
     )
     likes = PositiveIntegerField(default=0)
     views = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
