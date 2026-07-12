c8d9a2767e69dfefd769f0ddd096f2f1baf8295f
Amir Mohammad Adhami <amiradhamir@gmail.com>
2026-07-12 17:40:18 +0330

    feat: Create Accounts App feat: Create Blog App feat: Create Custom User Model in Accounts app

---
+++ accounts/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+
+
+# Base User Model and Base User Manager --------------- 1 ---------------
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError(_('Users must have username'))
+        if not email:
+            raise ValueError(_('Users must have email'))
+
+        user = self.model(username=username, email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class Base(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=30, unique=True, db_index=True)
+    email = models.EmailField(unique=True, db_index=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    is_author = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
